09713bd4705beb2b525b0fa0e5d2e0fbd7aca231
Luiz <doginuwu@gmail.com>
2022-01-11 20:40:13 -0300

    Added my informations on card

--- lib/main.dart
+++ lib/main.dart
@@ -9,44 +9,83 @@ class MyCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueAccent[400],
-        body: SafeArea(
+          backgroundColor: Colors.blueAccent[400],
+          body: SafeArea(
             child: Column(
-          // mainAxisSize: MainAxisSize.min,
-          verticalDirection: VerticalDirection.down,
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.all(20),
-              color: Colors.white,
-              child: Text("Hello, world"),
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/doginuwu.jpg'),
+                ),
+                Text(
+                  'Luiz (DoginUwU)',
+                  style: TextStyle(
+                    fontFamily: 'MochiyPopPOne',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'DESENVOLVEDOR FRONT-END',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 18,
+                    letterSpacing: 2,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: 250,
+                  height: 20,
+                  child: Divider(
+                    color: Colors.blueAccent[100],
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.blueAccent[400],
+                    ),
+                    title: Text(
+                      'doginuwu@gmail.com',
+                      style: TextStyle(
+                        color: Colors.blueAccent[400],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.language,
+                      color: Colors.blueAccent[400],
+                    ),
+                    title: Text(
+                      '@DoginUwU',
+                      style: TextStyle(
+                        color: Colors.blueAccent[400],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: double.infinity,
-              height: 100,
-              color: Colors.blue,
-              child: Text("New container ;3"),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: double.infinity,
-              height: 100,
-              color: Colors.blue,
-              child: Text("New container (again)"),
-            ),
-            Container(
-              width: double.infinity,
-            )
-          ],
-        )),
-      ),
+          )),
     );
   }
 }
