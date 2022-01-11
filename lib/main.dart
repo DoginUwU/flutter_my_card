02d0cd36cdd806bf31080df0e94558e141b853f0
Luiz <doginuwu@gmail.com>
2022-01-11 19:25:55 -0300

    initial styles fro Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyCard());
+}
+
+class MyCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepOrange,
-        body: Container(),
+        backgroundColor: Colors.blueAccent[400],
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.only(left: 30, top: 30),
+            color: Colors.white,
+            child: Text("Hello, world"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
